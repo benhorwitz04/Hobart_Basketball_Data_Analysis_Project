ae6c468ecb1ad3eaf942a3cea7fe054df67f5e59
benhorwitz04 <ben.horwitz04@gmail.com>
2025-04-17 18:15:21 -0400

    Remove ignored folders from tracking

--- scripts/unexplored_scripts/pts_off_to_and_oreb.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    ROUND(AVG(CASE WHEN team_id = 'HOB' THEN oreb END), 2) AS hob_oreb_game,
-    ROUND(AVG(CASE WHEN team_id = 'HOB' THEN pts_ch2 END), 2) AS hob_second_chance_pts,
-    ROUND(AVG(CASE WHEN team_id != 'HOB' THEN turnovers END), 2) AS opp_to_game,
-    ROUND(AVG(CASE WHEN team_id = 'HOB' THEN pts_to END), 2) AS hob_pts_off_to
-FROM team_stats;
